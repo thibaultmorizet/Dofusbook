90880280e0f541ae5acf79972c3928e5ceaafb31
Thibault <thibault@glanum.com>
2023-04-03 12:15:21 +0200

    This is a new commit for what I originally planned to be amended

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::fallback(function () {
+    return redirect('login');
+});
+
+
+require __DIR__.'/auth.php';
